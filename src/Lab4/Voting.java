903c4deb3363054f0d0af1be1f241e1a8d5e5d4f
Saadatmandalborz <82696872+Saadatmandalborz@users.noreply.github.com>
2021-04-17 17:55:59 +0430

    complete voting

---
+++ src/Lab4/Voting.java
@@ -0,0 +1,58 @@
+package Lab4;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class Voting {
+    private static final int SINGLE_ANSWER_MODE = 0;
+    private static final int MULTI_ANSWER_MODE = 0;
+    private int type;
+    private String question;
+    private ArrayList<Person> voters;
+    private HashMap<String, HashSet<Vote>> polls;
+
+    public Voting(int type, String question) {
+        this.type = type;
+        this.question = question;
+        voters = new ArrayList<>();
+        polls = new HashMap<>();
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void createPoll(String text)
+    {
+        polls.put(text,new HashSet<>());
+    }
+
+    public void vote(Person person, ArrayList<String> votes)
+    {
+        Date date = new Date();
+        SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+        String dateString = formatter.format(date);
+        if(type == SINGLE_ANSWER_MODE && polls.size() > 1)
+        {
+            System.out.println("Error, single mode!");
+            return;
+        }
+        if(voters.contains(person))
+        {
+            System.out.println("This person voted");
+            return;
+        }
+        voters.add(person);
+        for(String s : votes)
+        {
+            polls.get(s).add(new Vote(person,dateString));
+        }
+    }
+
+    public HashMap<String, HashSet<Vote>> getPolls() {
+        return polls;
+    }
+}
